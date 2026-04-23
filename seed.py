67ebafbfe57c0230ee7c79cc705b40c123769525
doluthegreat <akojededoluwamu@gmail.com>
2026-04-23 12:39:01 +0100

    updated

--- seed.py
+++ seed.py
@@ -1,9 +1,8 @@
 import os, json, uuid6, psycopg2, sys
 from utils import *
-import sys
-import json
 
 DATABASE_URL = os.environ.get("DATABASE_URL", "").replace("postgres://", "postgresql://")
+
 def seed(path):
     print("🔵 Loading file...")
 
@@ -25,26 +24,33 @@ def seed(path):
 
     c = conn.cursor()
 
+    values = []
+
     for p in data:
         if not isinstance(p, dict):
-            continue  
+            continue
 
-        name = p["name"].lower()
-        age = p["age"]
-        cid = p["country_id"]
-
-        c.execute("""
-            INSERT INTO profiles VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,NOW())
-            ON CONFLICT (name) DO NOTHING
-        """, (
-            str(uuid6.uuid7()), name, p["gender"], p["gender_probability"],
-            age, age_to_group(age),
-            cid, COUNTRY_ID_TO_NAME.get(cid, cid),
+        values.append((
+            str(uuid6.uuid7()),
+            p["name"].lower(),
+            p["gender"],
+            p["gender_probability"],
+            p["age"],
+            age_to_group(p["age"]),
+            p["country_id"],
+            COUNTRY_ID_TO_NAME.get(p["country_id"], p["country_id"]),
             p["country_probability"]
         ))
 
+    c.executemany("""
+        INSERT INTO profiles VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,NOW())
+        ON CONFLICT (name) DO NOTHING
+    """, values)
+
     conn.commit()
     conn.close()
-    print(f"Done! {len(data)} profiles processed.")  
+
+    print(f"Done! {len(values)} profiles processed.")
+
 if __name__ == "__main__":
     seed(sys.argv[1])
